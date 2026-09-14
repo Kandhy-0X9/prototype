78f39479afa0f3ee44f3a9ec3b3f9a539b332d3c
Kandhy-0X9 <kashengkandhy@gmail.com>
2026-09-14 15:11:32 -0400

    uuuuu

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-const signInButton = document.getElementById('sign-in-button');
+const signUpButton = document.getElementById('sign-up-button');
+signUpButton.addEventListener('click', () => {
+    console.log('Sign-up button was clicked.');
+    window.location.href = 'sign-in.html';
+});
 
+const signInButton = document.getElementById('sign-in-button');
 signInButton.addEventListener('click', () => {
     console.log('Sign-in button was clicked.');
+    window.location.href = 'mainpage.html';
 });
